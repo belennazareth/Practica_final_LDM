b65d06e51750282cc9857358357dea26405c018a
belennazareth <belennazareth29@gmail.com>
2022-05-15 04:36:50 +0200

    Modificacion request

--- request.py
+++ request.py
@@ -2,7 +2,9 @@ import requests
 
 ## Petición para la película Ponyo usando como parámetro el título
 
-r =requests.get("https://ghibliapi.herokuapp.com/films?title=Ponyo")
+
+title={'title':'Ponyo'}
+r =requests.get("https://ghibliapi.herokuapp.com/films", params=title)
 r.status_code
 r.text
 
