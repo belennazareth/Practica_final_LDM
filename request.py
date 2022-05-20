9527190b2c4c83540c72b1b78c18349d0e6b0cb6
belennazareth <belennazareth29@gmail.com>
2022-05-20 11:03:41 +0200

    cambio request

--- request.py
+++ request.py
@@ -3,16 +3,21 @@ import json
 import os
 
 url_base="https://ghibliapi.herokuapp.com/films"
+print()
 
 ## Petición para la película Ponyo usando como parámetro el título y dando como salida el título seguido de la descripción 
 
 title={'title':'Ponyo'}
+
 r =requests.get(url_base, params=title)
 if r.status_code == 200:
     doc=r.json()
     for elem in doc:
         print(elem["title"]+ " : " +elem["description"])
 
+print('''
+~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·
+''')
 
 ## Petición para mostrar las películas usando como parámetro el nombre de uno de los productores
 
@@ -21,16 +26,23 @@ producer={'producer':'Toshio Suzuki'}
 r = requests.get(url_base, params=producer)
 if r.status_code == 200:
     doc=r.json()
-    print()
-    print(elem["producer"])
+    print(elem["producer"] + ": ")
     for elem in doc:
-        print(" : " +elem["description"])
+        print(" - " + elem["title"])
+
+print('''
+~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·~*·
+''')
 
 ## Petición que muestra la información de las películas usando de parámetro director
 
-#director={'director':'Hayao Miyazaki'}
-#
-#r= requests.get("https://ghibliapi.herokuapp.com/films", params=director)
-#r.status_code
-#r.text
-#
+director={'director':'Hayao Miyazaki'}
+
+r = requests.get(url_base, params=director)
+if r.status_code == 200:
+    doc = r.json()
+    for elem in doc:
+        print(elem)
+        print('''
+                *.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*.·.*
+                ''')
